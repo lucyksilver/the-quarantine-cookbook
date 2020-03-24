69da98202f552d060dae0f733cfdc32b9f9ccb9e
Lucy Silver <lucy.silver2@gmail.com>
2020-03-24 19:12:47 +0000

    index controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: :show
+  # resources :users, only: :show
 
   resources :recipes do
-    resources :user_recipes, only: [:create]
-    resourses :ingredients, only: [:new, :create, :edit, :update]
-    resources :descriptions, only: [:new, :create, :edit, :update]
+  #   resources :user_recipes, only: [:create]
+  #   resourses :ingredients, only: [:new, :create, :edit, :update]
+  #   resources :descriptions, only: [:new, :create, :edit, :update]
   end
 
-  resources :user_recipes, only: [:destroy]
-  resources :ingredients, only: [:destroy]
-  resources :descriptions, only: [:destroy]
+  # resources :user_recipes, only: [:destroy]
+  # resources :ingredients, only: [:destroy]
+  # resources :descriptions, only: [:destroy]
 end
