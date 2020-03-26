5b99c988c7160b59fc4a7bc516ae443c3ec67745
Lucy Silver <lucy.silver2@gmail.com>
2020-03-26 15:49:29 +0000

    search on index

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,14 @@
 class Recipe < ApplicationRecord
+  include PgSearch::Model
   has_many :ingredients
   has_many :user_recipes
   has_many :ingredients
   has_one :method
   belongs_to :user
   has_one_attached :photo
+
+  pg_search_scope :search_by_ingredient, associated_against: {
+    ingredients: :name
+  }
+
 end
