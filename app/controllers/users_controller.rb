39a9d9d2555304ceeef9cefde1a9ad06725decd0
Lucy Silver <lucy.silver2@gmail.com>
2020-04-07 18:39:29 +0100

    user logic for saved and uploaded recipes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @user_recipes = @user.user_recipes
+
+    @first_user_recipes = @user.user_recipes.first(3)
+    if @user.user_recipes.count > 3
+      @remainder_recipes = @user.user_recipes.last(@user.user_recipes.count - 3)
+    end
+
+    @first_uploaded = @user.recipes.first(3)
+    if @user.recipes.count > 3
+      @remainder_uploaded = @user.recipes.last(@user.recipes.count - 3)
+    end
   end
 end
